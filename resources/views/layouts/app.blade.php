58004c01f26c7232532d57e3e7b63ece1acd68c4
Takagi Yuya <y.t.060201@gmail.com>
2019-06-10 15:27:47 +0900

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta property="og:title" content="pafevinia🦒 (共同運営ブログ)">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://pafevinia.herokuapp.com/">
+    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpJJanfJZATM0S0_Loydo_OhMR00pvim7N79gDk-CD4CZFQL9">
+    <meta property="og:description" content="pafeviniaの新しい記事→『{{ $article->title }}』" />
+    <meta name="twitter:card" content="summary">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
