0b32d5e50de53522744706e1b6f3b2132235cddb
Takagi Yuya <y.t.060201@gmail.com>
2019-06-10 18:47:52 +0900

    layout change

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,24 +4,38 @@
 <div class="container">
     <div class="row box p-3 mb-5">
         <div class="col-md-11 mx-auto">
+            <p>
+                <a href="{{ action('Admin\ProfileController@show', ['id' => $article->user->id]) }}" class="badge badge-secondary">
+                    @if ($article->user->profile != null && $article->user->profile->image_path != null)
+                        <img src="{{ $article->user->profile->image_path }}" class="image-mini mx-auto">
+                        {{ $article->user->profile->name }}
+                    @elseif ($article->user->profile != null && $article->user->profile->image_path == null)
+                        <img src="{{ asset('images/noprofileimage.jpg') }}" class="image-mini mx-auto">
+                        {{ $article->user->profile->name }}
+                    @elseif ($article->user->profile == null)
+                        <img src="{{ asset('images/noprofileimage.jpg') }}" class="image-mini mx-auto">
+                        {{ $article->user->name }}
+                    @endif
+                </a>
+            </p>
             <p><i class="far fa-clock"></i> {{ $article->created_at->format('Y/m/d/D') }}</p>
             <div class="image col-md-11 mx-auto">
                 @if ($article->image_path != null)
                     <img src="{{ $article->image_path }}" alt="" class="image-article mx-auto">
                 @endif
             </div>
-            <hr size="3" color="gray">
+            <hr>
             <h1>{{ $article->title }}</h1>
-            <hr size="3" color="gray">
+            <hr>
             <p>{!! $article->markdown_body !!}</p> <!-- HTMLをそのまま表示するため、エスケープをしないリテラルに変更 -->
-            <hr size="3" color="gray">
+            <hr size="5" color="gray">
             <br>
-            <ul class="snsbtniti2">
+            <ul class="snsbtniti2 mt-5 mb-5">
                 <li>
-                    <a href="https://twitter.com/intent/tweet?url={{ request()->fullUrl() }}" class="flowbtn12 fl_tw2"><i class="fab fa-twitter"></i><span>Twitterで記事をシェア</span></a>
+                    <a href="https://twitter.com/intent/tweet?url={{ request()->fullUrl() }}" class="flowbtn12 fl_tw2"><i class="fab fa-twitter"></i><span>Twitterでシェア</span></a>
                 </li>
                 <li>
-                    <a href="https://www.facebook.com/sharer.php?src=https://pafevinia.herokuapp.com/" class="flowbtn12 fl_fb2"><i class="fab fa-facebook-f"></i><span>Facebookで記事をシェア</span></a>
+                    <a href="https://www.facebook.com/sharer.php?src=https://pafevinia.herokuapp.com/" class="flowbtn12 fl_fb2"><i class="fab fa-facebook-f"></i><span>Facebookでシェア</span></a>
                 </li>
             </ul>
         </div>
