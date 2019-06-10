4eacf4d322b37103ea200097abb0cf9be098ba84
Takagi Yuya <y.t.060201@gmail.com>
2019-06-10 15:36:19 +0900

    meta

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -8,7 +8,7 @@
     <meta property="og:title" content="pafevinia🦒 (共同運営ブログ)">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://pafevinia.herokuapp.com/">
-    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpJJanfJZATM0S0_Loydo_OhMR00pvim7N79gDk-CD4CZFQL9">
+    <meta property="og:image" content="https://pafevinia.herokuapp.com/">
     <meta property="og:description" content="pafeviniaの新しい記事→『{{ $article->title }}』">
     <meta name="twitter:card" content="summary">
 
--- resources/views/layouts/nextpage_app.blade.php
+++ resources/views/layouts/nextpage_app.blade.php
@@ -8,7 +8,8 @@
     <meta property="og:title" content="pafevinia🦒 (共同運営ブログ)">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://pafevinia.herokuapp.com/">
-    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpJJanfJZATM0S0_Loydo_OhMR00pvim7N79gDk-CD4CZFQL9">
+    <meta property="og:image" content="https://pafevinia.herokuapp.com/">
+    <!-- 画像は投稿毎に対応させたい <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpJJanfJZATM0S0_Loydo_OhMR00pvim7N79gDk-CD4CZFQL9"> -->
     <meta property="og:description" content="pafeviniaの新しい記事→『{{ $article->title }}』" />
     <meta name="twitter:card" content="summary">
     <!--　これ必須？？　<meta name="twitter:site" content="@0201yu_ya">　　-->
